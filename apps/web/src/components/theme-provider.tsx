87778c73ccf85a3dd113af27529927ab17440247
Tiago Moreira <tiagomoreira@wearepixelmatters.com>
2026-03-04 12:23:16 +0000

    update theme-provider

--- apps/web/src/components/theme-provider.tsx
+++ apps/web/src/components/theme-provider.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes';
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+type Props = ThemeProviderProps & {
+  children: React.ReactNode;
+};
+
+export function ThemeProvider({ children, ...props }: Props) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
